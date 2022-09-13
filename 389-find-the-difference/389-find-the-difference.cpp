961d5ba82a404b5585d6ad4ac2920d2be9485752
surbhichaudhary <76531812+surbhichaudhary@users.noreply.github.com>
2022-09-13 22:32:45 +0530

    Time: 15 ms (5.64%), Space: 6.6 MB (67.45%) - LeetHub

---
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        unordered_map<char, int> mp;
+        
+        for( int i = 0 ; i < s.length() ; i++ ){
+            mp[s[i]]++;
+        }
+        for( int i = 0 ; i < t.length() ; i++ ){
+            if( !mp.count(t[i]) ){
+                return t[i];
+            }
+            mp[t[i]]--;
+            if( mp[t[i]] == 0 ){
+                mp.erase(t[i]);
+            }
+        }
+        return t[0];
+    }
+};
