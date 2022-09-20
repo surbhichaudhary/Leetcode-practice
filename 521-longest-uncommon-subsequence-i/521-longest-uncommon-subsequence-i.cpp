c8e5e054246d0a01ec69666be9c98a4d4978ebb7
surbhichaudhary <76531812+surbhichaudhary@users.noreply.github.com>
2022-09-20 08:08:54 +0530

    Time: 3 ms (34.66%), Space: 6.1 MB (81.39%) - LeetHub

---
+++ 521-longest-uncommon-subsequence-i/521-longest-uncommon-subsequence-i.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int findLUSlength(string a, string b) {
+        
+        if( a.compare(b) == 0 ){
+            return -1;
+        }
+        return max( a.length(), b.length() );
+    }
+};
