1c9b7013c5163377ada950349a82cd71c8eaa92b
surbhichaudhary <76531812+surbhichaudhary@users.noreply.github.com>
2023-01-24 15:16:49 +0530

    Time: 8 ms (34.21%), Space: 7.4 MB (13.55%) - LeetHub

--- 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
+++ 0535-encode-and-decode-tinyurl/0535-encode-and-decode-tinyurl.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
 
+    unordered_map< string, string > enc;
+    unordered_map< string, string > dec;
     // Encodes a URL to a shortened URL.
     string encode(string longUrl) {
-        return longUrl;
+        if( !enc.count(longUrl) ){
+            string curr = to_string( 1 + enc.size() );
+            enc[longUrl] = curr;
+            dec[curr] = longUrl;
+        }
+        return enc[longUrl];
     }
 
     // Decodes a shortened URL to its original URL.
     string decode(string shortUrl) {
-        return shortUrl;
+        return dec[shortUrl];
     }
 };
 
