5d762ec0149b63792c945de8cb962a9a8548ed5c
surbhichaudhary <76531812+surbhichaudhary@users.noreply.github.com>
2023-03-05 23:20:18 +0530

    Time: 197 ms (88.43%), Space: 72.7 MB (81.67%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.cpp
+++ 1345-jump-game-iv/1345-jump-game-iv.cpp
@@ -2,50 +2,52 @@ class Solution {
 public:
     
     int minJumps(vector<int>& arr) {
-        int n = arr.size(), ans =0;
         unordered_map< int, vector<int> > mp;
+        int n = arr.size(), ans = 0;
+        if( n <= 2 ){
+            return n-1; 
+        }
         
         for( int i = 0 ; i < n ; i++ ){
             mp[arr[i]].push_back(i);
         }
         
-        vector<bool> visit( n, false);
         queue<int> q;
-        q.push(0) ;
-        visit[0] = true;
+        q.push(0);
+        vector<int> time( n, INT_MAX );
+        time[0] = 0;
         
         while( !q.empty() ){
             int s = q.size();
-            for( int j = 0; j < s; j++ ){
+            
+            for( int i = 0 ; i < s ; i++ ){
                 int fr = q.front();
                 q.pop();
-                
-                if( fr == n-1 ) return ans;
-                
-                if( fr-1 >= 0 && !visit[fr-1] ){
+
+                if( fr == n-1 )
+                    return ans;
+
+                if( (fr > 0) && (time[fr-1] == INT_MAX) ){
+                    time[fr-1] = 1+time[fr];
                     q.push(fr-1);
-                    visit[fr-1] = true;
                 }
-                if( fr+1 < n && !visit[fr+1] ){
+                if( (fr < n-1) && (time[fr+1] == INT_MAX) ){
+                    time[fr+1] = 1+time[fr];
                     q.push(fr+1);
-                    visit[fr+1] = true;
                 }
-                
                 if( mp.count(arr[fr]) ){
-                    for( auto k : mp[arr[fr]] ){
-                        if( !visit[k] ){
-                            q.push(k);
-                            visit[k] = true;
+                    for( auto j : mp[arr[fr]] ){
+                        if( time[j] > time[fr]+1 ){
+                            q.push(j);
+                            time[j] = time[fr]+1;
                         }
                     }
-                    mp.erase(arr[fr]);
                 }
+                mp.erase( arr[fr]);
             }
             ans++;
         }
         
         return -1;
-        
     }
 };
-
